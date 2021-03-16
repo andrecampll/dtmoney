b864c0ef9b0b1b09d3e6fccd806dbfead757e7ed
André <andrevictor50@gmail.com>
2021-03-16 20:52:39 -0300

    Adding Summary and  Dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import theme from './styles/theme';
+import { Dashboard } from './components/Dashboard';
 
 export function App() {
   return (
     <ThemeProvider theme={theme}>
         <Header />
+        <Dashboard />
         <GlobalStyle />
     </ThemeProvider>
   );
