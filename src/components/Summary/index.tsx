064d306a91c2487bc562899dc06757f497995882
André <andrevictor50@gmail.com>
2021-04-30 10:03:49 -0300

    Finishing Transactions Summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,9 +8,31 @@ import totalImg from '../../assets/total.svg';
 import { Container } from "./styles";
 
 export function Summary() {
-  const data = useContext(TransactionsContext);
+  const { transactions } = useContext(TransactionsContext);
 
-  console.log(data);
+  // const totalIncome = transactions.reduce((acc, transaction) => {
+  //   if (transaction.type === 'deposit') {
+  //     return acc + transaction.amount;
+  //   }
+
+  //   return acc;
+  // }, 0);
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    };
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0,
+  });
 
   return (
     <Container>
@@ -20,7 +42,12 @@ export function Summary() {
           <img src={incomeImg} alt="Entradas"/>
         </header>
 
-        <strong>R$ 1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}
+        </strong>
       </div>
 
       <div>
@@ -29,7 +56,12 @@ export function Summary() {
           <img src={outcomeImg} alt="Saídas"/>
         </header>
 
-        <strong>- R$1000,00</strong>
+        <strong>
+          - {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws)}
+        </strong>
       </div>
 
       <div className="highlight-background">
@@ -38,7 +70,12 @@ export function Summary() {
           <img src={totalImg} alt="Total"/>
         </header>
 
-        <strong>R$1500,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}  
+        </strong>
       </div>
     </Container>
   );
